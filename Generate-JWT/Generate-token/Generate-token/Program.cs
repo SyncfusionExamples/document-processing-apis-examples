f8ff654819d238e86f4e06e213f24c0b3c7f686a
ManigandanShanmugam <manigandan.shanmugam@syncfusion.com>
2024-07-04 12:22:02 +0530

    Added the sample for JWT generation

---
+++ Generate-JWT/Generate-token/Generate-token/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+string privateKey = File.ReadAllText("jwtRS256.key");
+RSA rsa = RSA.Create();
+rsa.ImportFromPem(privateKey.ToCharArray());
+
+var tokenHandler = new JwtSecurityTokenHandler();
+var tokenDescriptor = new SecurityTokenDescriptor
+{
+    Issuer = "sample",
+    Audience = "audience",
+    Expires = DateTime.UtcNow.AddMinutes(30),
+    SigningCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha512)
+};
+
+var token = tokenHandler.CreateToken(tokenDescriptor);
+string tokenString = tokenHandler.WriteToken(token);
+
+Console.WriteLine("Generated JWT Token:");
+Console.WriteLine(tokenString);
